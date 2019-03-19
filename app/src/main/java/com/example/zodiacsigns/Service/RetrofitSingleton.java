2f21c282f77edb673507b8e3a19b6eef20fdd1fd
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-19 15:58:13 -0400

    Add retrofitsingleton class

---
+++ app/src/main/java/com/example/zodiacsigns/Service/RetrofitSingleton.java
@@ -0,0 +1,25 @@
+package com.example.zodiacsigns.Service;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitSingleton {
+
+    private static Retrofit instance;
+    private static final String BASE_URL = "https://raw.githubusercontent.com";
+
+    private RetrofitSingleton() {
+    }
+
+    public static Retrofit getInstance() {
+        if (instance == null) {
+            instance = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+        }
+        return instance;
+    }
+}
