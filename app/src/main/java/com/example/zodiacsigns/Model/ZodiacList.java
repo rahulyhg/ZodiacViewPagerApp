9c238362aced84a19eeecb33a5fe0578fdb996e5
Gregg Nicholas <Greggnicholas@pursuit.org>
2019-03-19 15:25:29 -0400

    Create model classes

---
+++ app/src/main/java/com/example/zodiacsigns/Model/ZodiacList.java
@@ -0,0 +1,16 @@
+package com.example.zodiacsigns.Model;
+
+import java.util.List;
+
+public class ZodiacList {
+
+    private List<ZodiacSign> zodiac;
+
+    public ZodiacList(List<ZodiacSign> zodiac) {
+        this.zodiac = zodiac;
+    }
+
+    public List<ZodiacSign> getZodiac() {
+        return zodiac;
+    }
+}
---
+++ app/src/main/java/com/example/zodiacsigns/Model/ZodiacSign.java
@@ -0,0 +1,26 @@
+package com.example.zodiacsigns.Model;
+
+public class ZodiacSign {
+    private String name;
+    private String number;
+    private String image;
+
+    public ZodiacSign(String name, String number, String image) {
+        this.name = name;
+        this.number = number;
+        this.image = image;
+    }
+
+
+    public String getZodiacName() {
+        return name;
+    }
+
+    public String getDateRange() {
+        return number;
+    }
+
+    public String getZodiacImage() {
+        return image;
+    }
+}
